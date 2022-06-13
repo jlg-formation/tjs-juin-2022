c88df53ecdeca8f387b3eef772e3046a7c60d778
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-13 16:10:27 +0200

    dev: logo header ongoing

--- front/src/layout/Header.tsx
+++ front/src/layout/Header.tsx
@@ -1,5 +1,15 @@
+import logo from "../logo.svg";
+import "./Header.css";
+
 function Header() {
-  return <div>Header works!</div>;
+  return (
+    <header className="Header">
+      <a href="#">
+        <img src={logo} alt="Logo Gestion Stock" />
+        <span>Gestion Stock</span>
+      </a>
+    </header>
+  );
 }
 
 export default Header;
