8b2c783d64fe85c517b4eefcfbaafb2a07b5b97f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-13 16:58:10 +0200

    dev: footer css
    we tried with styles attributes, but unfortunately styles cannot do everything we need (ex: a:hover)

--- front/src/layout/Footer.tsx
+++ front/src/layout/Footer.tsx
@@ -1,5 +1,20 @@
+import "./Footer.css";
+
 function Footer() {
-  return <div>Footer works!</div>;
+  return (
+    <footer
+      className="Footer"
+      style={{
+        backgroundColor: "#eee",
+        height: "3em",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <a href="#">Mentions Légales</a>
+    </footer>
+  );
 }
 
 export default Footer;
