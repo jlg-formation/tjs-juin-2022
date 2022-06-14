78d5fcdef3b962220a29da711cdfce215afc78d5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-14 17:16:47 +0200

    dev: added attributes articles

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,10 +2,14 @@ import { Article } from "../interfaces/Article";
 import { sleep } from "../utils";
 
 class API {
+  articles = [
+    { id: "1", name: "Tournevis", price: 1.23, qty: 100 },
+    { id: "2", name: "Marteau", price: 4.5, qty: 34 },
+  ];
   async retrieveAllArticles(): Promise<Article[]> {
     console.log("appel http");
     await sleep(2000);
-    return [{ id: "1", name: "Tournevis", price: 1.23, qty: 100 }];
+    return this.articles;
   }
 }
 
