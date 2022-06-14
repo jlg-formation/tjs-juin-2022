4c70463b212b8ef535382a4c2fdf5cc5c5f1cc47
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-14 12:19:33 +0200

    dev: css add

--- front/src/routes/Add.tsx
+++ front/src/routes/Add.tsx
@@ -1,5 +1,24 @@
 function Add() {
-  return <main>add works</main>;
+  return (
+    <main>
+      <h1>Ajouter un article</h1>
+      <form>
+        <label>
+          <div>Nom</div>
+          <input type="text" />
+        </label>
+        <label>
+          <div>Prix</div>
+          <input type="text" />
+        </label>
+        <label>
+          <div>Quantité</div>
+          <input type="text" />
+        </label>
+        <button className="primary">Ajouter</button>
+      </form>
+    </main>
+  );
 }
 
 export default Add;
