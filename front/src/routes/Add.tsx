c2c76ca287a0e284743df5d387b08e31deea3b73
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-15 09:29:00 +0200

    dev: added onSubmit

--- front/src/routes/Add.tsx
+++ front/src/routes/Add.tsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
+import { Article, NewArticle } from "../interfaces/Article";
+import api from "../services/api";
 
 function Add() {
-  const [name, setName] = useState("xxx");
+  const [name, setName] = useState("Truc");
+  const [price, setPrice] = useState(1);
+  const [qty, setQty] = useState(1);
 
   return (
     <main>
       <h1>Ajouter un article</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          (async () => {
+            try {
+              const article: NewArticle = {
+                name,
+                price,
+                qty,
+              };
+              // await api.addArticle(article);
+            } catch (err) {
+              console.log("err: ", err);
+            }
+          })();
+        }}
+      >
         <label>
           <div>Nom</div>
           <input
@@ -22,15 +42,30 @@ function Add() {
         </label>
         <label>
           <div>Prix</div>
-          <input type="text" />
+          <input
+            type="number"
+            value={price}
+            onChange={(event) => {
+              console.log("event: ", event);
+              const newPrice = +event.target.value;
+              console.log("newPrice: ", newPrice);
+              setPrice(newPrice);
+            }}
+          />
         </label>
         <label>
           <div>Quantité</div>
-          <input type="text" />
+          <input
+            type="number"
+            value={qty}
+            onChange={(event) => setQty(+event.target.value)}
+          />
         </label>
         <button className="primary">Ajouter</button>
       </form>
       <span>name: {name}</span>
+      <span>price: {price}</span>
+      <span>qty: {qty}</span>
     </main>
   );
 }
