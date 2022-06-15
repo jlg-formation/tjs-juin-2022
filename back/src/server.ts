d3e2f16920a0209ec9d823b191264458db6076c3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-15 11:46:10 +0200

    dev: hello world express

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,3 +1,12 @@
-console.log("About to start a server");
-const n: number = 4;
-console.log("n: ", n);
+import express from "express";
+
+const app = express();
+const port = 3500;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
